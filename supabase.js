d0679d96af08fc9f3dc81b36a444a7bb3c4e63f6
anhngoc27912-pixel <anhngoc27912@gmail.com>
2026-07-16 11:14:21 +0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,1 @@
-const SUPABASE_URL = "https://dqhojagrdtivexcaxcto.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_UTYUUbqF7HkHEhpfaPMBVQ_clDbJjK5";
 
-const supabase = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
-);
