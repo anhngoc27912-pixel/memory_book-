3a724869100cedc1d5776f825197354b451d2d1e
anhngoc27912-pixel <anhngoc27912@gmail.com>
2026-07-16 11:08:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const openButton = document.getElementById("openLetter");
 const letterForm = document.getElementById("letterForm");
-const sendButton = document.getElementById("sendLetter");
 
 openButton.addEventListener("click", () => {
     letterForm.classList.remove("hidden");
@@ -9,35 +8,4 @@ openButton.addEventListener("click", () => {
     letterForm.scrollIntoView({
         behavior: "smooth"
     });
-});
-
-sendButton.addEventListener("click", async () => {
-
-    const name = document.getElementById("name").value.trim();
-    const message = document.getElementById("message").value.trim();
-
-    if (message === "") {
-        alert("Please write something for AN 💛");
-        return;
-    }
-
-    const { error } = await supabase
-        .from("letters")
-        .insert([
-            {
-                name: name,
-                message: message
-            }
-        ]);
-
-    if (error) {
-        console.error(error);
-        alert("Something went wrong 😢");
-    } else {
-        alert("☀️ Your letter has found its way to AN.\n\nMay your kindness always shine back to you. 💛");
-
-        document.getElementById("name").value = "";
-        document.getElementById("message").value = "";
-    }
-
 });
