60f2f5932db6b1e193a611c078c12889bdcfd772
anhngoc27912-pixel <anhngoc27912@gmail.com>
2026-07-17 16:23:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,22 @@ const openButton = document.getElementById("openLetter");
 const letterForm = document.getElementById("letterForm");
 
 openButton.addEventListener("click", () => {
-  letterForm.classList.remove("hidden");
-  openButton.style.display = "none";
 
-  letterForm.scrollIntoView({
-    behavior: "smooth"
-  });
+    openButton.style.transform = "scale(.9)";
+    openButton.style.opacity = "0";
+
+    setTimeout(() => {
+
+        openButton.style.display = "none";
+
+        letterForm.classList.remove("hidden");
+
+        letterForm.classList.add("show");
+
+        letterForm.scrollIntoView({
+            behavior:"smooth"
+        });
+
+    },300);
+
 });
